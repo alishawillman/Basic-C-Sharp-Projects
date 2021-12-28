d2cd3ba8e812490ff235eca121a630de993ca8d0
alishawillman <72734094+alishawillman@users.noreply.github.com>
2021-12-28 01:35:56 -0700

    Created a simple program to determine if an individual can get car insurance using boolean logic.

---
+++ Basic_C#_Programs/BooleanLogic/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("What is your age?");
+        string age = Console.ReadLine();
+
+        Console.WriteLine("Have you ever had a DUI? Answer True or False.");
+        string dui = Console.ReadLine();
+
+        Console.WriteLine("How many speeding tickets do you have?");
+        string tickets = Console.ReadLine();
+
+        // Casting string to int
+        int personAge = Convert.ToInt32(age);
+        int personTickets = Convert.ToInt32(tickets);
+
+        // Casting string to bool
+        bool personDUI = bool.Parse(dui);
+
+
+        Console.WriteLine("Is this person qualified for car insurance?");
+        Console.ReadLine();
+        bool qualified = (personAge >= 15 && personDUI == false && personTickets <= 3);
+        Console.WriteLine(qualified);
+        Console.ReadLine();
+    }
+}
