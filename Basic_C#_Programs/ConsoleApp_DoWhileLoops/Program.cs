cffc9628f4b0bbd6cf62f0f0c22bc4eda37faa4b
alishawillman <72734094+alishawillman@users.noreply.github.com>
2021-12-29 01:47:28 -0700

    Created a do while and while loop to guess my favorite color.

---
+++ Basic_C#_Programs/ConsoleApp_DoWhileLoops/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+
+namespace ConsoleApp_DoWhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess my favorite color!");
+            String color = Console.ReadLine();
+            bool isGuessed = color == "red";
+
+            // Do While Loop
+            do
+            {
+                switch (color)
+                {
+                    case "blue":
+                        Console.WriteLine("You guessed blue, but that's incorrect! Try again!");
+                        Console.WriteLine("Guess my favorite color!");
+                        color = Console.ReadLine();
+                        break;
+                    case "purple":
+                        Console.WriteLine("You guessed purple, but that's incorrect! Try again!");
+                        Console.WriteLine("Guess my favorite color!");
+                        color = Console.ReadLine();
+                        break;
+                    case "green":
+                        Console.WriteLine("You guessed green, but that's incorrect! Try again!");
+                        Console.WriteLine("Guess my favorite color!");
+                        color = Console.ReadLine();
+                        break;
+                    case "yellow":
+                        Console.WriteLine("You guessed yellow, but that's incorrect! Try again!");
+                        Console.WriteLine("Guess my favorite color!");
+                        color = Console.ReadLine();
+                        break;
+                    case "red":
+                        Console.WriteLine("You guessed red! That's correct!");
+                        isGuessed = true;
+                        break;
+                    case "orange":
+                        Console.WriteLine("You guessed orange, but that's incorrect! Try again!");
+                        Console.WriteLine("Guess my favorite color!");
+                        color = Console.ReadLine();
+                        break;
+                    default:
+                        Console.WriteLine("That's incorrect! Try again!");
+                        Console.WriteLine("Guess my favorite color!");
+                        color = Console.ReadLine();
+                        break;
+                }
+            }
+            //While loop 
+            while (!isGuessed); //Shorthand for (isGuessed == false)
+      
+
+            Console.Read();
+        }
+    }
+}
