15dae4d64974aa5e3204f3abf82f4c5742d216ae
alishawillman <72734094+alishawillman@users.noreply.github.com>
2022-01-03 18:27:20 -0700

    Created a console app using arrays and lists. The user selects an index to display and it throws an error if it is outside of the range. I used an if else statement for 1 error and I used try catch for the other 2.

--- Basic_C#_Programs/ArrayLists/Program.cs
+++ Basic_C#_Programs/ArrayLists/Program.cs
@@ -9,22 +9,62 @@ class Program
     {
         //Array of strings
         string[] stringArray = { "Alisha", "Joey", "Luna", "Zeus", "Gus"};
-
-        //Ask the user to select an index of the Array
+        //Ask user to select index of stringArray
+        Console.WriteLine("Select an index of the string array.");
+        int index = int.Parse(Console.ReadLine());
+        
+        //Error messgae if index doesn't exist using if else statement
+        if (index <= stringArray.Length)
+        {
+            Console.WriteLine(stringArray[index]);
+        }
+        else
+        {
+            Console.WriteLine("That index does not exist!");
+        }
 
         //Array of integers
         int[] numArray = { 126, 32, 67, 89, 327 };
-
         //Ask the user to select an index of the Array
+        Console.WriteLine("Select an index of the number array.");
+        int index1 = int.Parse(Console.ReadLine());
 
-        //Error message for index that doesn't exist
+        //Error message for index that doesn't exist using try catch 
+        try
+        {
+            Console.WriteLine(numArray[index1]);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+
 
         //List of strings
         List<string> stringList = new List<string>();
-        stringList.Add("Hello");
-        stringList.Add("World");
+        stringList.Add("Hello,");
+        stringList.Add("World!");
+        stringList.Add("Have");
+        stringList.Add("a");
+        stringList.Add("great");
+        stringList.Add("day!");
 
         //Ask the user to select an index of the list
+        Console.WriteLine("Select an index of the list.");
+        int index2 = int.Parse(Console.ReadLine());
+
+        //Error message using try catch again
+        try
+        {
+            Console.WriteLine(stringList[index2]);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        Console.Read();
     }
 }
 
